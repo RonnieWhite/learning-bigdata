b3adf79cf7d51e50e36d114a353ad1bdda376997
bai.chenghui <bai.chenghui@datatom.com>
2020-01-16 17:33:41 +0800

    初始化提交

---
+++ src/bin/day05_hdfs/HdfsClientDemo.java
@@ -0,0 +1,17 @@
+package bin.day05_hdfs;
+
+import java.net.URI;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HdfsClientDemo {
+}
