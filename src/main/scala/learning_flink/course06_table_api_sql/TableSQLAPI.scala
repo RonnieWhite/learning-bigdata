f20837b4e3e3e9706eaa7fadbf6f718880220405
R.White <15018408620@163.com>
2020-03-02 21:59:24 +0800

    进度更新 tableapi

--- src/main/scala/learning_flink/course06_table_api_sql/TableSQLAPI.scala
+++ src/main/scala/learning_flink/course06_table_api_sql/TableSQLAPI.scala
@@ -1,10 +1,24 @@
 package main.scala.learning_flink.course06_table_api_sql
 
 import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.table.api.Table
+import org.apache.flink.table.api.scala.BatchTableEnvironment
+import org.apache.flink.api.scala._
+import org.apache.flink.types.Row
 
 
 object TableSQLAPI {
   def main(args: Array[String]): Unit = {
-    println("hello")
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+    val tableEnv: BatchTableEnvironment = BatchTableEnvironment.create(env)
+    val csv: DataSet[person] = env.readCsvFile[person]("E:/data/flink/input/person.csv", ignoreFirstLine = true)
+    val table: Table = tableEnv.fromDataSet(csv)
+    tableEnv.registerTable("person", table)
+    val result: Table = tableEnv.sqlQuery("SELECT avg(age) age FROM person")
+    tableEnv.toDataSet[Row](result).print()
   }
+
+
+  case class person(name: String, age: Int, job: String)
+
 }
