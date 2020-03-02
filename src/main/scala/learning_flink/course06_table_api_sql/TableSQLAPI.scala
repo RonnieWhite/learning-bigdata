e8f7eb599bb1bc5955e6fdf5dc7784c63deef1b7
bai.chenghui <bai.chenghui@datatom.com>
2020-03-02 18:00:52 +0800

    jar包

---
+++ src/main/scala/learning_flink/course06_table_api_sql/TableSQLAPI.scala
@@ -0,0 +1,10 @@
+package main.scala.learning_flink.course06_table_api_sql
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+
+
+object TableSQLAPI {
+  def main(args: Array[String]): Unit = {
+    println("hello")
+  }
+}
