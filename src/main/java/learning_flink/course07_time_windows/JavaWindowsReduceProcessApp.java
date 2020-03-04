fe7cf7171dbad7cad525c3cefe017e601b7cc2ec
R.White <15018408620@163.com>
2020-03-04 23:29:11 +0800

    进度更新--connectors

--- src/main/java/learning_flink/course07_time_windows/JavaWindowsReduceProcessApp.java
+++ src/main/java/learning_flink/course07_time_windows/JavaWindowsReduceProcessApp.java
@@ -2,13 +2,16 @@ package main.java.learning_flink.course07_time_windows;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 
-public class JavaWindowsReduceApp {
+public class JavaWindowsReduceProcessApp {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStreamSource<String> data = env.socketTextStream("localhost", 9999);
@@ -23,15 +26,25 @@ public class JavaWindowsReduceApp {
             }
         }).keyBy(0)
                 .timeWindow(Time.seconds(5))
-                .reduce((value1, value2) -> {
-                    return new Tuple2<>(value1.f0, value1.f1 + value2.f1);
-                })
+//                .reduce((value1, value2) -> {
+//                    return new Tuple2<>(value1.f0, value1.f1 + value2.f1);
+//                })
 //                .reduce(new ReduceFunction<Tuple2<Integer, Integer>>() {
 //                    @Override
 //                    public Tuple2<Integer, Integer> reduce(Tuple2<Integer, Integer> value1, Tuple2<Integer, Integer> value2) throws Exception {
 //                        return new Tuple2<>(value1.f0, value1.f1 + value2.f1);
 //                    }
 //                })
+                .process(new ProcessWindowFunction<Tuple2<Integer, Integer>, Object, Tuple, TimeWindow>() {
+                    @Override
+                    public void process(Tuple tuple, Context context, Iterable<Tuple2<Integer, Integer>> elements, Collector<Object> out) throws Exception {
+                        long count = 0;
+                        for (Tuple2<Integer, Integer> in : elements) {
+                            count++;
+                        }
+                        out.collect("Window:" + context.window() + "count:" + count);
+                    }
+                })
                 .print();
         env.execute();
     }
