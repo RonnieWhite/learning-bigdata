a2caf4eabdd98c6ed152b832732dcfbfa014887a
bai.chenghui <bai.chenghui@datatom.com>
2020-03-06 20:18:19 +0800

    java se的一些总结

--- src/main/java/practise/review/review.java
+++ src/main/java/practise/review/review.java
@@ -19,11 +19,11 @@ public class review {
         list.sort(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                if (o1 > o2){
+                if (o1 > o2) {
                     return 1;
-                }else if (o1.equals(o2)){
+                } else if (o1.equals(o2)) {
                     return 0;
-                }else {
+                } else {
                     return -1;
                 }
             }
