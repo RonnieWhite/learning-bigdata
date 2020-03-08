b3eb8775ccd07b460ef073088030a1c7d92537c9
R.White <15018408620@163.com>
2020-03-08 21:40:16 +0800

    进度更新--fink启动

--- src/main/java/practise/review/review.java
+++ src/main/java/practise/review/review.java
@@ -1,12 +1,12 @@
 package main.java.practise.review;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 
 public class review {
-    static {
-        System.out.println("hello java");
-    }
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
@@ -31,5 +31,34 @@ public class review {
         for (int i : list) {
             System.out.println(i);
         }
+    }
+
+    @Test
+    public void runHash() {
+        String a = "abc";
+        int code = a.hashCode();
+        int hashCode = code ^ (code >>> 16);
+        System.out.println(code);
+        System.out.println(hashCode);
+    }
+
+    @Test
+    public void runW() {
+        System.out.println(17 % 3);
+    }
+
+    @Test
+    public void hash() {
+        new HashSet<>();
+        int h;
+        String key = "white";
+        h = key.hashCode();
+        int s = h >>> 16;
+        int c = (key == null) ? 0 : h ^ s;
+        int i = 2 & 3;
+        System.out.println(i);
+//        System.out.println(c);
+//        System.out.println(h);
+//        System.out.println(s);
     }
 }
