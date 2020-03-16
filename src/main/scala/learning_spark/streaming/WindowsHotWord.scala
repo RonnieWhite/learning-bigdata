041a8663e99000ea66524d122cbbfc071abf5f10
R.White <15018408620@163.com>
2020-03-16 22:19:35 +0800

    spark更新-热词项目实战

--- src/main/scala/learning_spark/streaming/WindowsHotWord.scala
+++ src/main/scala/learning_spark/streaming/WindowsHotWord.scala
@@ -1,7 +1,7 @@
 package main.scala.learning_spark.streaming
 
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.dstream.ReceiverInputDStream
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object WindowsHotWord {
@@ -9,6 +9,24 @@ object WindowsHotWord {
     val conf: SparkConf = new SparkConf().setAppName("whw").setMaster("local[2]")
     val sc: StreamingContext = new StreamingContext(conf, Seconds(5))
     val hotWordsStream: ReceiverInputDStream[String] = sc.socketTextStream("localhost", 9999)
-    hotWordsStream.map(line => (line.split(" ")(1), 1))
+    val searchWordPairsDStream: DStream[(String, Int)] = hotWordsStream.map(line => (line.split(" ")(1), 2))
+    val searchWordCountsDStream: DStream[(String, Int)] = searchWordPairsDStream.reduceByKeyAndWindow(
+      (v1: Int, v2: Int) => v1 + v2,
+      Seconds(60),
+      Seconds(10))
+    val finalDStream: DStream[(String, Int)] = searchWordCountsDStream.transform(searchWordCountsRDD => {
+      val topThreeSearchWordCounts: Array[(String, Int)] = searchWordCountsRDD.map(tuple => (tuple._2, tuple._1))
+        .sortByKey(false: Boolean)
+        .map(tuple => (tuple._2, tuple._1))
+        .take(3)
+      for (tuple <- topThreeSearchWordCounts) {
+        println(tuple)
+      }
+      searchWordCountsRDD
+    })
+    finalDStream.print()
+    sc.start()
+    sc.awaitTermination()
+    sc.stop()
   }
 }
