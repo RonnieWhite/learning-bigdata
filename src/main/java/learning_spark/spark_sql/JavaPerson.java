6830b15968aa6c1ddff50a06d13c5603ebb20d14
R.White <15018408620@163.com>
2020-03-18 23:18:08 +0800

    spark更新-笔记、spark sql

---
+++ src/main/java/learning_spark/spark_sql/JavaPerson.java
@@ -0,0 +1,24 @@
+package main.java.learning_spark.spark_sql;
+
+import java.io.Serializable;
+
+public class JavaPerson implements Serializable {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
