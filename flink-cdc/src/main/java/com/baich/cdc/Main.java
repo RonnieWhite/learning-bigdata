ae69bc1a7f2d69fbb5a9e4c7e277f9d52faa44b6
baich <15519087989@163.com>
2021-11-20 17:27:50 +0800

    cdc demo

--- flink-cdc/src/main/java/com/baich/cdc/Main.java
+++ flink-cdc/src/main/java/com/baich/cdc/Main.java
@@ -1,5 +1,14 @@
 package com.baich.cdc;
 
+
+//import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
+//import com.alibaba.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+
+import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
+import com.alibaba.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
 /**
  * Created By IDEA.
  * Author : BaiCH
@@ -10,7 +19,25 @@ package com.baich.cdc;
  * version : v1.0
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("=======");
+    public static void main(String[] args) throws Exception {
+        SourceFunction<String> sourceFunction = MySQLSource.<String>builder()
+                .hostname("192.168.10.130")
+                .port(3306)
+                .databaseList("ods")// monitor all tables under ods database
+//                .tableList("t_test")
+                .username("root")
+                .password("123456")
+                .deserializer(new StringDebeziumDeserializationSchema()) // converts SourceRecord to String
+                .build();
+
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env
+                .addSource(sourceFunction)
+                .print().setParallelism(1); // use parallelism 1 for sink to keep message ordering
+        env.execute();
+
+
     }
 }
